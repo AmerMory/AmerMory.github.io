07fc106ee92cd4de833c763114ae6b104866403e
Mohamed amer <amer.mohamed57@yahoo.com>
2020-07-14 23:40:19 +0200

    Weather App

---
+++ weather-journal-app/server.js
@@ -0,0 +1,58 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+const bodyParser = require('body-parser');
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+const { listeners } = require('process');
+app.use(cors())
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+
+const port = 800;
+const server = app.listen(port, listening)
+
+function listening (e) {
+    console.log('Server Running');
+    console.log(`Running on localhost: ${port}`);
+}
+
+const weatherData = [];
+
+app.get('/all', getData)
+
+function getData (req, res) {
+    res.send(weatherData)
+    console.log(weatherData);
+};
+
+app.post('/', addWeather);
+
+function addWeather (req, res) {
+    newEntry = {
+        temperature: getData,
+        date: newDate,
+        userResponse: feelings
+    }
+
+    weatherData.push(newEntry)
+    res.send(weatherData)
+    console.log(weatherData);
+
+}
+
